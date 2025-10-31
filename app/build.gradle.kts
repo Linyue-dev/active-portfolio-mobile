ec74a71f8827c324ae9af9e4d06e69873146e49f
linyueCS-IT <6197673@johnabbottcollege.net>
2025-10-31 18:50:48 -0400

    feat(profile): display user's first name, last name, email, role, and photo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
